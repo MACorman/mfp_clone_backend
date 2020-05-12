f28942e34b86031126700ff601a2ad6a61b44863
MACorman <madelinecorman@gmail.com>
2020-05-12 16:48:06 -0400

    added create action and params to create food_diaries

--- app/controllers/food_diaries_controller.rb
+++ app/controllers/food_diaries_controller.rb
@@ -4,4 +4,15 @@ class FoodDiariesController < ApplicationController
         food_diaries = FoodDiary.all
         render json: food_diaries
     end
+
+    def create
+        food_diary = FoodDiary.create(food_diary_params)
+        render json: food_diary
+    end 
+
+    private
+
+    def food_diary_params
+        params.require(:food_diary).permit(:food_id, :diary_id, :category)
+    end
 end
